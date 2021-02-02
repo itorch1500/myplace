6a11837e2412dfc79b50aa914e67a8cf37b7bf7f
itorch1500 <58251049+itorch1500@users.noreply.github.com>
2021-02-02 15:11:21 +0900

    Add files via upload

---
+++ B2869.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class B2869 {
+
+	public static void main(String[] args) throws Exception{
+		
+		
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	
+	int A = Integer.parseInt(st.nextToken());
+	int B = Integer.parseInt(st.nextToken());
+	int V = Integer.parseInt(st.nextToken());
+	
+	int result = (V-B) / (A-B);
+	
+	if((V-B) % (A-B)!=0)
+	{
+		result+=1;
+	}
+	
+	System.out.print(result);
+	
+		
+		
+		
+		
+		
+	
+}
+}
